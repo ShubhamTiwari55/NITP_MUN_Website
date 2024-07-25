ef4745996beeceeb3f48a34c654fe2d3462d6edc
Satyam1345 <Satyamkumar1345@gmail.com>
2024-07-25 21:15:12 +0530

    Navbar Done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,72 +1,67 @@
-"use client" ;
+"use client";
 import React from "react";
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { SiGooglemaps } from "react-icons/si";
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const Footer = () => {
-  // const [visits, setVisits] = useState(0);
-
-  // useEffect(() => {
-  //   const fetchVisits = async () => {
-  //     const response = await fetch("/api/visit?url=home");
-  //     const data = await response.json();
-  //     setVisits(data.visits);
-  //   };
-
-  //   fetchVisits();
-  //   console.log(visits);
-  // }, []);
   return (
-    <footer className="bg-gray-900 max-w-screen mx-auto px-4 py-6 md:py-8">
-      <div className="flex flex-col md:flex-row md:justify-between items-center mx-0">
-        {/* Social Icons */}
-        <div className="flex space-x-4 md:mb-0">
-          <a href="https://www.instagram.com/expresso_nitp/" target="blank">
-            <FaInstagram className="text-white w-6 h-6 md:w-8 md:h-8" />
-          </a>
-          <a href="https://www.facebook.com/expresso.nitp/" target="blank">
-            <FaFacebook className="text-white w-6 h-6 md:w-8 md:h-8" />
-          </a>
+    <>
+      <div className="mx-auto grid grid-cols-2 md:flex md:flex-row bg-[#171f44] bg-gradient-to-t text-white max-w-screen px-6 py-1 md:py-4 md:justify-between">
+        <div className="flex my-2 object-contain items-center justify-center">
+          <Image src="/logomun2.png" alt="Logo" width={160} height={130} />
+        </div>
+
+        <div className="flex flex-col text-white py-4 items-center text-center md:justify-evenly">
+          <h1 className="font-bold font-colona">CONTACT US</h1>
           <a
-            href="https://www.linkedin.com/company/expressonitp/"
+            href="mailto:expressoclub@nitp.ac.in"
             target="blank"
+            className="my-2"
           >
-            <FaLinkedin className="text-white w-6 h-6 md:w-8 md:h-8" />
+            <IoIosMail className="text-white w-6 h-6 md:w-10 md:h-10" />
           </a>
-          <a href="mailto:expressoclub@nitp.ac.in" target="blank">
-            <IoIosMail className="text-white w-6 h-6 md:w-8 md:h-8" />
-          </a>
-          <a href="https://maps.app.goo.gl/ochD6thH9G93RZJK8" target="blank">
-            <SiGooglemaps className="text-white w-6 h-6 md:w-8 md:h-8" />
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-white text-sm font-semibold text-center md:text-left mt-4 mb-2 hover:underline">
-          &copy; Expresso NIT Patna 2024
-        </div>
-
-        {/* Address and Logo */}
-        <div className="flex items-center">
           <div className="text-white text-xs">
-            <p className="mb-1 text-'Antonio'">
+            <p className="mb-1 font-Antonio">
               Student Activity Centre (SAC), NIT Patna
             </p>
             <p>Ashok Rajpath, Patna, Bihar - 800005</p>
           </div>
-          <div className="ml-4">
-            <Link href="/" alt=":)">
-              <Image src="/assets/logo.svg" alt="Logo" width={80} height={80} />
-            </Link>
+        </div>
+
+        <div className="flex flex-col py-4 items-center text-center justify-evenly">
+          <h1 className="font-bold font-colona">FOLLOW US</h1>
+          <h1></h1><h1></h1><h1></h1><h1></h1> <h1></h1>
+          <div className="flex flex-row justify-center space-x-4 mt-2">
+            <a href="https://www.instagram.com/nitpatna_mun/" target="blank">
+              <FaInstagram className="text-white w-6 h-6 md:w-10 md:h-10" />
+            </a>
+            <a href="https://www.linkedin.com/company/expressonitp/" target="blank">
+              <FaLinkedin className="text-white w-6 h-6 md:w-10 md:h-10" />
+            </a>
+            <a
+              href="https://www.instagram.com/expresso_nitp/"
+              target="blank"
+            >
+              <FaInstagram className="text-white w-6 h-6 md:w-10 md:h-10" />
+            </a>
           </div>
         </div>
+
+        <div className="flex flex-col text-white justify-between py-2 items-center text-center">
+          <h1 className="font-Colona font-bold pt-4">ORGANISER</h1>
+          <Image
+            src="/expresso.png"
+            className="pb-2"
+            alt="expresso"
+            width={80}
+            height={80}
+          />
+        </div>
       </div>
-      {/* <p className="text-white">Visits Number:{visits}</p> */}
-    </footer>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        colona: ['Colona', 'sans-serif'],
+      },
+      backgroundImage:{
+        'gradient-to-t': 'linear-gradient(to top, #000829 , #38387C)',
+
       },
     },
   },
