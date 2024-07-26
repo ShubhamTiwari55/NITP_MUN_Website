bd4bcf9e03d578fd968f3a4f64a1c507a52c2328
Lakshya <harshitlucky2004@gmail.com>
2024-07-26 18:52:51 +0530

    secratriat

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Navbar from '@/components/Navbar';
+
 import "./globals.css";
 
+import Secratriat from "./Secratriat";
 
 export default function Home() {
   return (
-    <h1> OM GANESHAY NAMAH </h1>
+    <>
+    <Navbar/>
+
+    <Secratriat />
+   
+    </>
   );
 }
---
+++ constant/teamdata.js
@@ -0,0 +1,56 @@
+const teamDetails = {
+    secretrians: {
+      members: [
+        {
+          id: '1',
+          name: 'LAKSHYA PARASHAR',
+          designation: 'GENERAL SECREATORY',
+          linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+          instagram: 'https://www.instagram.com/laksh.yya/',
+          image:'logomun2.png',
+        },
+        {
+            id: '2',
+            name: 'LAKSHYA PARASHAR',
+            designation: 'GENERAL SECREATORY',
+            linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+            instagram: 'https://www.instagram.com/laksh.yya/',
+            image:'logomun2.png',
+          },
+          {
+            id: '3',
+            name: 'LAKSHYA PARASHAR',
+            designation: 'GENERAL SECREATORY',
+            linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+            instagram: 'https://www.instagram.com/laksh.yya/',
+            image:'logomun2.png',
+          },
+          {
+            id: '4',
+            name: 'LAKSHYA PARASHAR',
+            designation: 'GENERAL SECREATORY',
+            linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+            instagram: 'https://www.instagram.com/laksh.yya/',
+            image:'logomun2.png',
+          },
+          {
+            id: '5',
+            name: 'LAKSHYA PARASHAR',
+            designation: 'GENERAL SECREATORY',
+            linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+            instagram: 'https://www.instagram.com/laksh.yya/',
+            image:'logomun2.png',
+          },
+          {
+            id: '6',
+            name: 'LAKSHYA PARASHAR',
+            designation: 'GENERAL SECREATORY',
+            linkedin: 'https://www.linkedin.com/in/lakshya-parashar-65156b301/',
+            instagram: 'https://www.instagram.com/laksh.yya/',
+            image:'logomun2.png',
+          }
+      ]
+    }
+  };
+  
+  export default teamDetails;
