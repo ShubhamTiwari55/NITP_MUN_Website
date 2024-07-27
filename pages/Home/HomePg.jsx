36bc181a7d86adc3d9bba32679a7e6062e7543ca
Satyam1345 <Satyamkumar1345@gmail.com>
2024-07-27 23:31:53 +0530

    HomePg, Committees

--- pages/Home/HomePg.jsx
+++ pages/Home/HomePg.jsx
@@ -1,94 +1,117 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-function RuleBook() {
+function HomePg() {
+  const [showFullDescriptionExpresso, setShowFullDescriptionExpresso] = useState(false);
+  const [showFullDescriptionNitPatna, setShowFullDescriptionNitPatna] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [showFullDescriptionExpresso, showFullDescriptionNitPatna]);
+
+  const handleReadMoreExpresso = () => setShowFullDescriptionExpresso(true);
+  const handleReadLessExpresso = () => setShowFullDescriptionExpresso(false);
+
+  const handleReadMoreNitPatna = () => setShowFullDescriptionNitPatna(true);
+  const handleReadLessNitPatna = () => setShowFullDescriptionNitPatna(false);
+
+  const fullTextExpresso =
+    `Welcome to Expresso - The Literary & Art Club NIT Patna! We are the vibrant community where creativity in literature and public speaking thrives. Whether you're into writing, debating, or any other form of expression, this is your place to explore and showcase your talents.
+Expresso is more than just a club; it's a journey of self-discovery and growth. Since our inception, we've provided a platform for students to nurture their literary and oratory skills. With numerous events, workshops, and competitions, we inspire and engage creative minds, helping them flourish in their craft.
+Over the years, Expresso has built a legacy of fostering literary and public speaking talents. Our members have won numerous events across various colleges, bringing accolades and recognition to NIT Patna. We take pride in our diverse and dynamic community, where every voice is heard, and every talent is celebrated.Get ready for an exciting event this September! We're bringing back NIT Patna MUN 2.0, one of our most eagerly anticipated events. This conference offers a platform for debate, diplomacy, and intellectual engagement. Join us for a thrilling experience and be part of this amazing adventure!`;
+
+  const shortTextExpresso = fullTextExpresso.split(" ").slice(0, 50).join(" ") + "...";
+
+  const fullTextNitPatna =
+    `NIT Patna, established in 1886, is one of the oldest engineering institutions in India. Located in Patna, Bihar, the institute is renowned for its engineering and technological education. It has consistently been a leader in academic excellence and research, providing high-quality education and fostering innovation.
+    The institute is committed to holistic development, with a focus on engineering, science, and technology. It offers a wide range of undergraduate, postgraduate, and doctoral programs. NIT Patna is known for its vibrant campus life, state-of-the-art infrastructure, and a strong emphasis on research and development.
+    With a rich history and a reputation for producing outstanding graduates, NIT Patna is a hub for intellectual and technical advancement. The institute continues to contribute significantly to the field of engineering and technology, nurturing future leaders and innovators.`;
+
+  const shortTextNitPatna = fullTextNitPatna.split(" ").slice(0, 50).join(" ") + "...";
+
   return (
     <>
-      <div className="bg-gray-900 text-white p-20">
-        <section className="mb-10">
-          <div className="flex items-center mb-4">
-            <div className="">
-              <div className="text-7xl font-bold h-[45px] w-[45px] flex items-center justify-center text-gray-700">
-                01
-              </div>
-            </div>
-            <div className="relative text-lg font-bold text-[#FBD784]">
-              <div className="absolute left-[50px] top--5 flex items-center">
-                <div className="h-[20px] w-[50px] flex items-center justify-center">
-                  _______
-                </div>
-              </div>
-              <div className="absolute left-[120px] top-[5px] flex items-center">
-                <div className="h-[20px] w-[30px]">MOTIONS</div>
-              </div>
-            </div>
+      <div className="flex flex-col bg-[#000829] px-4 py-6">
+        {/* About NIT Patna Section */}
+        <div className="relative w-full max-w-screen-lg mx-auto text-white rounded-3xl p-6 mb-6"
+              style={{ backgroundImage: `url('/bg2.png')` }}
+        >
+          <div className="flex flex-col items-start">
+            <span className="text-5xl mb-2 font-semibold text-[#FBD784]">ABOUT</span>
+            <span className="text-2xl font-futura underline md:text-3xl">NIT PATNA</span>
           </div>
-
-          <div className="relative mb-6 h-[10px] w-[13px] top-0 left-2">
-            <h2 className="text-4xl font-bold">Moderated</h2>
-          </div>
-
-          <div className="relative mb-6 h-[10px] w-[13px] top-0 left-2">
-            <h2 className="text-4xl font-bold">Caucus</h2>
-          </div>
-
-          <div
-            className="relative mt-8 h-[234px] w-[1300px] p-10 top-10"
-            style={{ backgroundColor: "#0F121C" }}
-          >
-            <p>NIT Patna MUN is a premier Model United Nations event organized by the Expresso Art and Literary Club, providing a platform for young minds to engage in diplomacy, leadership, and global citizenship. We're a team of dedicated individuals passionate about empowering young individuals to become global leaders and change-makers. Our event is a transformative experience that fosters intellectual growth, negotiation skills, and responsible global citizenship. We believe in the power of diplomacy and dialogue to shape a better future, and we're committed to delivering an exceptional experience that exceeds expectations. With a focus on leadership, global citizenship, and intellectual growth, we're dedicated to making a positive impact on the world, one delegate at a time. Through our efforts, we aim to create a ripple effect of kindness, compassion, and understanding that resonates globally. By bringing together young minds from diverse backgrounds, we strive to break down cultural barriers, promote mutual understanding, and inspire collective action for a brighter tomorrow.</p>
-          </div>
-        </section>
-
-        <section className="relative h-screen pt-0  pl-0">
-          <div className="absolute top-[70px] right-[200px] flex flex-col items-end space-y-4">
-            <div className="flex items-center mb-4">
-              <div className="flex-shrink-0">
-                <div className="text-7xl font-bold h-[45px] w-[45px] flex items-center justify-center text-gray-700">
-                  02
-                </div>
-              </div>
-
-              <div className="relative text-lg font-bold text-[#FBD784] ml-6 flex items-center space-x-8">
-                <div
-                  className="h-[20px] w-[50px] flex items-center justify-center"
-                  style={{
-                    marginLeft: "20px",
-                    position: "relative",
-                    top: "5px",
-                  }}
-                >
-                  _______
-                </div>
-                <div
-                  className="flex items-center space-x-4"
-                  style={{ position: "relative", top: "13px" }}
-                >
-                  <div
-                    className="h-[10px] w-[90px] flex items-center justify-center"
-                    style={{ position: "relative", left: "10px" }}
+          <div className="mt-4 text-left md:text-xl">
+            <p className="p-0 w-full"
+            
+            >
+              {isMobile && !showFullDescriptionNitPatna ? shortTextNitPatna : fullTextNitPatna}
+            </p>
+            {isMobile && (
+              <div className="text-center mt-4">
+                {showFullDescriptionNitPatna ? (
+                  <span
+                    className="cursor-pointer text-xl text-blue-600 hover:underline"
+                    onClick={handleReadLessNitPatna}
                   >
-                    RESOLUTION
-                  </div>
-                  <div
-                    className="h-[10px] w-[90px] flex items-center justify-center"
-                    style={{ position: "relative", left: "10px" }}
+                    Read Less
+                  </span>
+                ) : (
+                  <span
+                    className="cursor-pointer text-xl text-blue-600 hover:underline"
+                    onClick={handleReadMoreNitPatna}
                   >
-                    WRITING
-                  </div>
-                </div>
+                    Read More
+                  </span>
+                )}
               </div>
-            </div>
-            <div className="relative mb-6 h-[10px] w-[13px] top-[0] right-20">
-              <h2 className="text-4xl font-bold">Fixed</h2>
-            </div>
-            <div className="relative mb-6 h-[10px] w-[13px] top-[2px] right-20">
-              <h2 className="text-4xl font-bold">Topics</h2>
-            </div>
+            )}
           </div>
-        </section>
+        </div>
+
+        {/* About Expresso Section */}
+        <div className="relative w-full max-w-screen-lg mx-auto text-white rounded-3xl p-6 mb-6"
+              style={{ backgroundImage: `url('/bg2.png')` }}
+>
+          <div className="flex flex-col items-start">
+            <span className="text-5xl mb-2 font-semibold text-[#FBD784]">ABOUT</span>
+            <span className="text-2xl font-futura underline md:text-3xl">EXPRESSO</span>
+          </div>
+          <div className="mt-4 text-left md:text-xl">
+            <p className="p-0 w-full">
+              {isMobile && !showFullDescriptionExpresso ? shortTextExpresso : fullTextExpresso}
+            </p>
+            {isMobile && (
+              <div className="text-center mt-4">
+                {showFullDescriptionExpresso ? (
+                  <span
+                    className="cursor-pointer text-xl text-blue-600 hover:underline"
+                    onClick={handleReadLessExpresso}
+                  >
+                    Read Less
+                  </span>
+                ) : (
+                  <span
+                    className="cursor-pointer text-xl text-blue-600 hover:underline"
+                    onClick={handleReadMoreExpresso}
+                  >
+                    Read More
+                  </span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default RuleBook;
+export default HomePg;
