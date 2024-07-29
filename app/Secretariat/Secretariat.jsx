9b3d7ec8de0ec61250c4d61806a7e5bbff9f4a3c
ShubhamTiwari55 <stofficial059@gmail.com>
2024-07-29 23:15:34 +0530

    secretariat page added

--- app/Secretariat/Secretariat.jsx
+++ app/Secretariat/Secretariat.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Card from '@/components/Card';
+import cardData from '@/constant/CardData';
 
 const Secretariat = () => {
+  const members = cardData.secretrians.members;
+
   return (
-    <div>Secretariat</div>
-  )
+    <div
+      className="container mx-auto px-4"
+      style={{
+        backgroundImage: 'url(Bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <div className="flex flex-wrap -mx-4">
+        {members.map((member, index) => (
+          <div key={index} className="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
+            <Card member={member} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Secretariat
+export default Secretariat;
